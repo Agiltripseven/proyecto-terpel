87cc7a477113c81ccbb34c6f1ad573fb7df38dba
Andres Uru <mr.uru198@gmail.com>
2023-11-05 12:07:12 -0500

    Primer commit de Andres

---
+++ src/components/Nombre.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Nombre = () => {
+  return (
+    <div>
+        <h1>Soy Andres</h1>
+
+    </div>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import { Nombre } from './components/nombre'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Nombre/>
+  </React.StrictMode>,
+)
