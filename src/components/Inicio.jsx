25063e02314c0c14c661cefaea0c37c66af6d926
Andres Uru <mr.uru198@gmail.com>
2023-11-14 18:53:16 -0500

    Union Inicio y Banner

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -17,7 +17,20 @@ import aliado6 from '../assets/img/aliado6.png'
 import aliado7 from '../assets/img/aliado7.png'
 import aliado9 from '../assets/img/aliado9.png'
 import aliado10 from '../assets/img/aliado10.png'
-import Banner from './Banner'
+
+import '../assets/style/Banner.css'
+
+import altoque from '../assets/img/altoque.jpg'
+import servicios from '../assets/img/servicios.jpg'
+import estaciones from '../assets/img/estaciones.png'
+import puntos from '../assets/img/puntos.jpg'
+import voltex from '../assets/img/voltex.jpg'
+import Alpine from 'alpinejs'
+ 
+window.Alpine = Alpine
+ 
+Alpine.start()
+
 
 const Inicio = () => {
   return (
@@ -47,7 +60,50 @@ const Inicio = () => {
         <div className='figura1'></div>
         <div className='figura2'></div>
       </main>
-      <Banner/>
+      <div className='carrousel'>
+        <hr />
+        <h1 class="title">Datos revelantes</h1>
+        <div class="slider" x-data="{start: true, end: false}">
+        <div class="slider__content" x-ref="slider" x-on:scroll="$refs.slider.scrollLeft == 0 ? start = true : start = false; Math.abs(($refs.slider.scrollWidth - $refs.slider.offsetWidth) - $refs.slider.scrollLeft) < 5 ? end = true : end = false;">
+            <div class="slider__item">
+            <img class="slider__image" src={altoque} alt="Image"/>
+            <div class="slider__infoUno">
+                <h2>+120 Tiendas altoque</h2>
+            </div>
+            </div>
+            <div class="slider__item">
+            <img class="slider__image" src={servicios} alt="Image"/>
+            <div class="slider__infoDos">
+                <h2>+15 Electrolineras</h2>
+            </div>
+            </div>
+            <div class="slider__item">
+            <img class="slider__image" src={estaciones} alt="Image"/>
+            <div class="slider__infoTres">
+                <h2>+2000 Estaciones Terpel</h2>
+            </div>
+            </div>
+            <div class="slider__item">
+            <img class="slider__image" src={puntos} alt="Image"/>
+            <div class="slider__infoCuatro">
+                <h2>+2 m inscritos vive terpel</h2>
+            
+            </div>
+            </div>
+            <div class="slider__item">
+            <img class="slider__image" src={voltex} alt="Image"/>
+            <div class="slider__infoCinco">
+                <h2>Terpel Voltex</h2>
+            </div>
+            </div>
+        </div>
+        <div class="slider__nav">
+            <button class="slider__nav__button" x-on:click="$refs.slider.scrollBy({left: $refs.slider.offsetWidth * -1, behavior: 'smooth'});" x-bind:class="start ? '' : 'slider__nav__button--active'">Previous</button>
+            <button class="slider__nav__button" x-on:click="$refs.slider.scrollBy({left: $refs.slider.offsetWidth, behavior: 'smooth'});" x-bind:class="end ? '' : 'slider__nav__button--active'">Next</button>
+        </div>
+        </div>
+        <hr />
+    </div>
       <div className='parallax'>
       <section class="image"></section>
       <section class="text_parallax">
---
+++ src/components/Prueba.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Prueba = () => {
+  return (
+    <div>
+        <h1>esto es una prueba</h1>
+        <p>esto esta</p>
+    </div>
+  )
+}
+
+export default Prueba
